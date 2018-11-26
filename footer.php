c5432e11dd877752884688a516ca16b252052519
lizzie marie <elizabethmariewillett@gmail.com>
2018-11-26 10:42:36 -0800

    my first commit

---
+++ footer.php
@@ -0,0 +1,45 @@
+			<!-- footer -->
+			<footer class="footer" role="contentinfo">
+
+				<!-- copyright -->
+				<section id="footersection">
+					<div id="footerthird1">
+						<p><strong>Lizzie Marie</strong><br>is a UX/UI Designer</p>
+					</div>
+					<div id="footerthird2">
+						<p><strong>Email</strong><br>hi(a)lizziemariew.com</p>
+					</div>
+					<div id="footerthird3">
+						<p><strong>Social</strong>
+						<br>
+							<a href="https://www.instagram.com/lizziemariew/" target="_blank">Instagram,</a>
+							<a href="https://github.com/lizziemarie" target="_blank">Github,</a>
+							<a href="https://www.linkedin.com/in/elizabethmwillett/" target="_blank">LinkedIn,</a>
+							<a href="https://www.pinterest.com/lizziemariew/" target="_blank">Pinterest</a>
+						</p>
+					</div>
+				</section>
+				<!-- /copyright -->
+
+			</footer>
+			<!-- /footer -->
+
+		</div>
+		<!-- /subwrapper -->
+	</div>
+	<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+
+		<!-- analytics -->
+		<script>
+		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		ga('send', 'pageview');
+		</script>
+
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+
+		<!--animate links-->
+		<link rel="stylesheet"
+	  href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css"
+	  integrity="sha384-OHBBOqpYHNsIqQy8hL1U+8OXf9hH6QRxi0+EODezv82DfnZoV7qoHAZDwMwEJvSw"
+	  crossorigin="anonymous">
+	  <!-- or -->
+	  <link rel="stylesheet"
+	  href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
+	  integrity="sha384-OHBBOqpYHNsIqQy8hL1U+8OXf9hH6QRxi0+EODezv82DfnZoV7qoHAZDwMwEJvSw"
+	  crossorigin="anonymous">
+
+		<?php wp_head(); ?>
+		<script>
+        // conditionizr.com
+        // configure environment tests
+        conditionizr.config({
+            assets: '<?php echo get_template_directory_uri(); ?>',
+            tests: {}
+        });
+        </script>
+
+	</head>
+	<body <?php body_class(); ?>>
+
+		<!-- wrapper -->
+		<div class="wrapper">
+
+			<!-- header -->
+			<header class="header clear" role="banner">
+
+					<!-- logo -->
+					<div class="logo">
+						<a href="<?php echo home_url(); ?>">
+							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+							<h2>lizzie marie</h2>
+						</a>
+					</div>
+					<!-- /logo -->
+
+					<!-- nav -->
+					<nav class="nav" role="navigation">
+						<?php html5blank_nav(); ?>
+					</nav>
+					<!-- /nav -->
+
+			</header>
+			<!-- /header -->
